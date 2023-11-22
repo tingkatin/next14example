2b41f75cd9f9de6babed1181b50140eb43a57e79
tingkatin <info.tingkatin@gmail.com>
2023-11-23 02:58:04 +0700

    Iniital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,7 @@
+import '@/app/ui/global.css';
+import '@/app/ui/fonts';
+import { manrope } from '@/app/ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${manrope.className} antialiased`}>{children}</body>
     </html>
   );
 }
